5ae16ee55faa1c6930bc0fd2f7625e875b11285a
mikan3rd <mikan.no.kikoudhi@gmail.com>
2018-01-06 23:43:53 +0900

    delete conda

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ from bottle import route, run
 
 @route("/")
 def hello_world():
-        return "hello world"
+    return "hello world"
 
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
